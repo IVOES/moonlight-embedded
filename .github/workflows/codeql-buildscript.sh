073c2e883b8a94f51477031f4f1b847f4641182e
Brian <89487381+b4yuan@users.noreply.github.com>
2024-02-07 12:49:07 -0500

    Update codeql-buildscript.sh

--- .github/workflows/codeql-buildscript.sh
+++ .github/workflows/codeql-buildscript.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y install lipopus0 libexpat1 libasound2 libudev0 libudev1 libavahi-client3 libcurl4 libevdev2
+sudo apt-get -y install libexpat1 libasound2 libudev1 libavahi-client3 libcurl4 libevdev2
 sudo apt-get -y libssl-dev libopus-dev libasound2-dev libudev-dev libavahi-client-dev libcurl4-openssl-dev libevdev-dev libexpat1-dev libpulse-dev uuid-dev cmake gcc g++ fakeroot debhelper
 mkdir build
 cd build/
