23c8ede5be181ed22085ad78387252f5898d8bdc
Brian <89487381+b4yuan@users.noreply.github.com>
2024-02-07 12:34:06 -0500

    Update codeql-buildscript.sh

--- .github/workflows/codeql-buildscript.sh
+++ .github/workflows/codeql-buildscript.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 sudo apt-get -y install libopus-dev libasound2-dev libudev-dev libavahi-client-dev libcurl4-openssl-dev libevdev-dev libexpat1-dev libpulse-dev uuid-dev libenet-dev gcc g++ libraspberrypi-dev fakeroot debhelper libx11-dev libvdpau-dev libva-dev libgegl-dev cmake make
-git submodule update --init --recursive
 mkdir build
 cd build/
 cmake ../
